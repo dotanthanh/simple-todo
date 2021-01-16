485f23f81458bc7bfa473a7b38f4c8be39b4d242
Thanh Do <tanthanhjan17@gmail.com>
2021-01-16 13:58:03 +0700

    wip, initial components
    Signed-off-by: Thanh Do <tanthanhjan17@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Card, Checkbox, Container, createMuiTheme, Divider, Grid, IconButton, TextField, ThemeProvider } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+      >
+        {[1,2,3,4,5,6,7].map(x => (
+          <Grid item xs={12} sm={6} md={4} lg={3} xl={2} >  
+            <ToDoItem />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
+
+const ToDoItem = () => (
+  <Card>
+    <Box>
+      <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='center'>
+        <Checkbox />
+        <TextField
+          id="datetime-local"
+          type="datetime-local"
+          focused={false}        
+          defaultValue="2017-05-24T10:30"
+          InputProps={{ disableUnderline: true }}
+          disabled
+        />
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+      <Divider />
+      <Box padding={2}>
+        <TextField
+          defaultChecked={true}
+          defaultValue='keke'
+          focused={false}
+          multiline
+          fullWidth
+          InputProps={{ disableUnderline: true }}
+        />
+      </Box>
+    </Box>
+  </Card>
+)
 
 export default App;
